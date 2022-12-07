e9bf6624fa33f8d188977c00709d836e89a24918
abd0ali <99900392+abd0ali@users.noreply.github.com>
2022-12-07 21:17:03 +0200

    Update basetests.java

--- src/main/java/base/basetests.java
+++ src/main/java/base/basetests.java
@@ -22,32 +22,3 @@ public class basetests {
 
 }
 
-//        WebElement searchInput = driver.findElement(By.name("q"));
-//        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
-//        wait();
-//        WebElement searchInput = driver.findElement(By.name("q"));
-//
-//        searchInput.sendKeys("giant panda");
-//        WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
-//
-//        searchButton.click();
-//        List<WebElement> resultLinks = driver.findElements(By.cssSelector("div.results_links_deep a.result__a"));
-//
-//       // wait.until(ExpectedConditions.titleContains("giant panda"));
-//      //  wait.until(ExpectedConditions.titleContains("giant panda"));
-//
-//      //  wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.results_links_deep a.result__a")));
-//
-//        for (WebElement link : resultLinks) {
-//            assertTrue(link.getText().matches("(?i).*panda.*"));
-//        }
-
-//            driver.get("https://the-internet.herokuapp.com/");
-//                    driver.manage().window().maximize();
-//                    System.out.println(driver.getTitle());
-//                    WebElement A= driver.findElement(By.linkText("Shifting Content"));
-//                    A.click();
-//                    WebElement B= driver.findElement(By.linkText("Example 1: Menu Element"));
-//                    B.click();
-//                    List<WebElement>links = driver.findElements(By.cssSelector("ul > li"));
-//        System.out.println(links.size());
